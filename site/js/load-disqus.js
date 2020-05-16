1f6269074b17d25fb3990d0826ec9a8a602830e5
huhuhang <huhuhang@users.noreply.github.com>
2020-05-16 16:52:53 +0800

    add site files

---
+++ site/js/load-disqus.js
@@ -0,0 +1,18 @@
+var disqus = {
+  load: function disqus() {
+    var disqus_shortname = "huhuhang"; // 修改为自己的 Disqus 名称
+    if (typeof DISQUS !== "object") {
+      (function() {
+        var s = document.createElement("script");
+        s.async = true;
+        s.type = "text/javascript";
+        s.src = "//" + disqus_shortname + ".disqus.com/embed.js";
+        (
+          document.getElementsByTagName("HEAD")[0] ||
+          document.getElementsByTagName("BODY")[0]
+        ).appendChild(s);
+      })();
+      $("#load-disqus").remove(); // 加载后自动删除
+    }
+  }
+};
